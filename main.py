ab566c6f848600c0ca8123712d9e14484e912f3d
Antonio Albuquerque <carlosalbuquerque359@gmail.com>
2022-05-08 23:19:58 -0300

    making request on esp server, using global ip so esp can send request

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, json, request
+import requests
+from time import sleep
 
 api = Flask(__name__)
-host, port = ("127.0.0.1", 8080)
+host, port = ("0.0.0.0", 8080)
 
 @api.route('/', methods=['GET'])
 def index():
@@ -14,5 +16,14 @@ def log():
     print("request.data", request)
     return json.dumps("log recived")
 
+# while (True):
+#     # sleep(1000)
+#     print("making get request")
+#     print(requests.patch("http://192.168.4.1/start").content)
+
 if __name__ == '__main__':
     api.run(host=host, port=8080)
+    # while (True):
+    #     # sleep(1000)
+    #     print("making get request")
+    #     print(requests.get("http://192.168.4.1/").content)
