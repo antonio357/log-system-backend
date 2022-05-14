bc09ecae0da2c6a8aa83f7fe1a30d86eac7d032f
antonio357 <carlosalbuquerque359@gmail.com>
2022-05-14 14:25:27 -0300

    testing number of msgs

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import websocket
-import _thread
-import time
-import rel
-
+import websocket, time, rel
 
 ip, port = ("192.168.0.113", 81)
 adrr = f"{ip}:{port}"
 url = f"ws://{adrr}"
 
-counter = 0
-def on_message(ws, message):
-    global counter
-    print(f"message num {counter}")
-    counter = counter + 1
+def current_milli_time():
+    return round(time.time() * 1000)
+
+msgs = []
+corrupted_msgs = []
+last_time = current_milli_time()
+def on_message(ws, msg):
+    global last_time
+    now = current_milli_time()
+    response_time = now - last_time
+    if len(msg) == 256: msgs.append((msg, response_time))
+    else: corrupted_msgs.append((msg, response_time))
+    last_time = now
 
 def on_error(ws, error):
     print(error)
@@ -21,7 +25,7 @@ def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 def on_open(ws):
-    print("Opened connection")
+    print("Connected to esp")
 
 if __name__ == "__main__":
     # websocket.enableTrace(True)
@@ -34,5 +38,8 @@ if __name__ == "__main__":
     ws.run_forever(dispatcher=rel)  # Set dispatcher to automatic reconnection
     rel.signal(2, rel.abort)  # Keyboard Interrupt
     rel.dispatch()
+
+mean_time = (sum([i[-1] for i in msgs]) + sum([i[-1] for i in corrupted_msgs])) / (len(msgs) + len(corrupted_msgs))
+print(f"received msgs: {len(msgs)} corrupted_msgs: {len(corrupted_msgs)} mean time: {mean_time}")
 
 # https://github.com/websocket-client/websocket-client
