49ccb5ee91dc34f3de7957688b37b8490d7149e9
antonio357 <carlosalbuquerque359@gmail.com>
2022-05-13 22:27:29 -0300

    testando header com informação do tamanho do dado

--- main.py
+++ main.py
@@ -7,30 +7,38 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET, socket.S
 s.connect(addr)
 print("connected to esp")
 
-init = "init"
-end = "end"
-lastIndicator = end
+max_len = 9999
+message_header = "log-header:9999."
+message_header_len = 17
 data = ""
 logs = []
 incomplete_data = []
+
+
+
+log_data = ""
+log_header_len = 16
+log_header = ""
+
 try:
     while True:
-        msg = s.recv(124)
-        # print(f"msg = {msg}") # 1024 tamanho do dado em bytes
-        msg = msg.decode("utf-8")
-        print(f"msg = {msg}")
-        if msg == init:
-            if lastIndicator == end:
-                data = ""
-                lastIndicator = init
-            else:
-                incomplete_data.append(data)
-                data = ""
-        elif msg == end:
-            logs.append(data)
-            data = ""
-            lastIndicator = end
-        else: data += msg
+        # reading log_header
+        msg = s.recv(message_header_len).decode("utf-8")
+        diff = log_header_len - len(log_header)
+        log_header += msg[:diff]
+        log_data += msg[diff:] # header and data may mix
+        if len(log_header) == log_header_len or len(log_data) > 0: # reading data
+            print(f"log_header = {log_header}")
+            expected_len = int(log_header.replace('.', '')[11:]) - len(log_data)
+            log_data += s.recv(expected_len).decode("utf-8")
+            # in the end
+            logs.append(log_data)
+            log_data = ""
+            log_header = ""
+
 except KeyboardInterrupt:
-    print(logs)
-    print(data)
+    pass
+    # print(logs)
+    # print(data)
+finally:
+    print(f"logs received = {len(logs)}")
