3e141194599111040ce695789768921b290f19cb
antonio357 <carlosalbuquerque359@gmail.com>
2022-07-09 17:24:18 -0300

    just testing

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from sniffer_logs_connection import SnifferLogsConnection
 
 
 if __name__ == "__main__":
-    snifferLogsConnection = SnifferLogsConnection(lifeTimeSeconds=5)
-    snifferLogsConnection.startLogs()
+    snifferLogsConnection = SnifferLogsConnection()
+    for i in range(2):
+        snifferLogsConnection.startLogs()
+        sleep(8)
+        snifferLogsConnection.stopLogs()
+        sleep(60*10)
 
-    while True:
-        sleep(1)
-        print(f"\nMain thread: {datetime.now()}\n")
+    # react https://www.youtube.com/watch?v=azvcvbeRZ08&ab_channel=WebDevJunkie ->
+    # https://github.com/codyseibert/youtube/tree/master/realtime-chart-websockets
+    # while True:
+    #     sleep(1)
+    #     print(f"\nMain thread: {datetime.now()}\n")
