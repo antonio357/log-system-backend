49150373fb29dd14756885ba9f45cf83ac7c8cf0
antonio357 <carlosalbuquerque359@gmail.com>
2022-07-09 04:21:15 -0300

    organizing code

--- sniffer_logs_connection.py
+++ sniffer_logs_connection.py
@@ -6,72 +6,79 @@ from time import sleep, time
 
 class SnifferLogsConnection:
     def __init__(self, lifeTimeSeconds=None):
-        self.name = "sniffer"
-        self.logsStatus = LogsStatus()
-        self.websocket = None
-        self.gotLatestLogs = False
-        self.thread = Thread(target=self.connect)
-        self.thread.start()
-        # lifeTime is just for tests
-        if lifeTimeSeconds:
-            Thread(target=self.stopLogsAfterSeconds, kwargs={"seconds": lifeTimeSeconds}).start()
+        self._name = "sniffer"
+        self._logsStatus = LogsStatus()
 
-    def connect(self):
+        self._websocket = None
+        self._thread = Thread(target=self._connect)
+        self._thread.start()
+        self._waitConnection()
+
+        # lifeTimeSeconds is just for tests
+        if lifeTimeSeconds:
+            Thread(target=self._stopLogsAfterSeconds, kwargs={"seconds": lifeTimeSeconds}).start()
+
+    def _connect(self):
         self.websocket = WebSocketApp(LogsConn.URL.value,
-                                      on_open=self.onOpen,
-                                      on_message=self.onMessage,
-                                      on_error=self.onError,
-                                      on_close=self.onClose)
+                                      on_open=self._onOpen,
+                                      on_message=self._onMessage,
+                                      on_error=self._onError,
+                                      on_close=self._onClose)
         self.websocket.run_forever()
 
-    def closeConnection(self):
-        self.websocket.close()
+    def _waitConnection(self):
+        startTime = self._nowTimeInSeconds()
+        while not self._isConnected():
+            if self._nowTimeInSeconds() - startTime > 5:
+                raise TimeoutError(f"{self._name} error: took more than 5 seconds to connect")
 
-    def startLogs(self):
-        print(f"{self.name} starting logs")
-        self.logsStatus.reset()
-        self.gotLatestLogs = False
-        self.waitConnection()
-        self.websocket.send(LogsConn.START_LOGS_CMD.value)
-
-    def stopLogs(self):
-        print(f"{self.name} stoping logs")
-        self.websocket.send(LogsConn.STOP_LOGS_CMD.value)
-        Thread(target=self.waitLatestLogs).start()
-
-    def stopLogsAfterSeconds(self, seconds):
-        sleep(seconds)
-        self.stopLogs()
-
-    def waitLatestLogs(self):
-        sleep(1)
-        self.logsStatus.printStatus()
-
-    def onMessage(self, websocket, message):
-        self.logsStatus.checkMsg(message)
-
-    def onOpen(self, websocket):
-        print(f"{self.name} connected")
-        self.logsStatus.printStatus()
-
-    def onError(self, websocket, error):
-        print(f"{self.name} error")
-        self.logsStatus.printStatus()
-
-    def onClose(self, websocket, close_status_code, close_message):
-        print(f"{self.name} closed connection with close_status_code = {close_status_code}, close_message = {close_message}")
-        self.logsStatus.printStatus()
-
-    def nowTimeInSeconds(self):
-        return int(time())
-
-    def isConnected(self):
+    def _isConnected(self):
         if self.websocket and self.websocket.sock.connected:
             return True
         return False
 
-    def waitConnection(self):
-        startTime = self.nowTimeInSeconds()
-        while not self.isConnected():
-            if self.nowTimeInSeconds() - startTime > 5:
-                raise TimeoutError(f"{self.name} error: took more than 5 seconds to connect")
+    def _closeConnection(self):
+        self.websocket.close()
+
+    def _onOpen(self, websocket):
+        self._printStrings("connected")
+        self._logsStatus.printStatus()
+
+    def _onMessage(self, websocket, message):
+        self._logsStatus.checkMsg(message)
+
+    def _onError(self, websocket, error):
+        self._printStrings("error")
+        self._logsStatus.printStatus()
+
+    def _onClose(self, websocket, close_status_code, close_message):
+        self._printStrings(f"closed connection with " 
+                           f"close_status_code = {close_status_code}, "
+                           f"close_message = {close_message}")
+        self._logsStatus.printStatus()
+
+    def startLogs(self):
+        self._printStrings("")
+        print(f"{self._name} starting logs")
+        self._logsStatus.reset()
+        self.websocket.send(LogsConn.START_LOGS_CMD.value)
+
+    def stopLogs(self):
+        self._printStrings("stopping logs")
+        self.websocket.send(LogsConn.STOP_LOGS_CMD.value)
+        Thread(target=self._waitLatestLogs).start()
+
+    def _waitLatestLogs(self):
+        sleep(1)
+        self._logsStatus.printStatus()
+
+    def _nowTimeInSeconds(self):
+        return int(time())
+
+    # test is just for tests
+    def _stopLogsAfterSeconds(self, seconds):
+        sleep(seconds)
+        self.stopLogs()
+
+    def _printStrings(self, string):
+        print(f"{self._name} {string}")
