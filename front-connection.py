c35b5254756dbcb07f85716603a2972cbe75c4bb
antonio357 <carlosalbuquerque359@gmail.com>
2022-07-10 23:38:07 -0300

    atempt 1 not working event loop error

--- front-connection.py
+++ front-connection.py
@@ -1,15 +1,43 @@
 import asyncio
 import websockets
+from threading import Thread
+from time import sleep
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message + " returning msg back")
+class FrontServer:
+    def __inti__(self):
+        self.name = "front server"
+        self.thread = None
+        self.websocket = websockets.serve(self._onMessge, "localhost", 8765)
 
-async def main():
-    async with websockets.serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    async def _onMessge(self, websocket, path):
+        async for message in websocket:
+            await websocket.send(message + " returning msg back")
+            print(f"message = {message}")
 
+    def _isUp(self):
+        return True
 
-asyncio.run(main())
+    def _waitStarUp(self):
+        while not self._isUp():
+            continue
 
+    def _initServer(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.websocket)
+        loop.run_forever()
+
+    def runServer(self):
+        self.thread = Thread(target=self._initServer)
+        self.thread.start()
+        self._waitStarUp()
+
+frontServer = FrontServer()
+frontServer.runServer()
+
+print("got here")
+while True:
+    sleep(1)
+    print("running main thread")
+
+# https://www.youtube.com/watch?v=lv0oEnQY1pM&ab_channel=Vuka
 # https://websockets.readthedocs.io/en/stable/index.html
